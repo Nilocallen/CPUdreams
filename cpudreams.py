ad099568d4e58afeed9e7058214d46210c158a58
Nilocallen <colinallen0217@gmail.com>
2023-01-18 02:39:16 -0500

    changed colors, new 'gui' package

--- cpudreams.py
+++ cpudreams.py
@@ -11,6 +11,7 @@
 import pyglet as pyg
 from modules import and_circuit, not_circuit
 
+
 display = pyg.canvas.Display()
 screen = display.get_default_screen()
 screen_width = screen.width
@@ -19,15 +20,37 @@ screen_height = screen.height
 app_window = pyg.window.Window(resizable=True)
 app_window.maximize()
 
-background = pyg.image.SolidColorImagePattern((169, 169, 169, 255)).create_image(screen_width, screen_height)
+background = pyg.image.SolidColorImagePattern((70, 68, 72, 255)).create_image(screen_width, screen_height)
 
 main_batch = pyg.graphics.Batch()
 
-circuit_store = pyg.shapes.Rectangle(10, 10, screen_width - 20, 80, color=(54, 69, 79, 255),
-                                     batch=main_batch)
-circuit_name = pyg.gui.TextEntry("Name", 10, screen_height - 90, screen_width - 20, color=(54, 69, 79, 255),
-                                 batch=main_batch, text_color=(169, 169, 169, 255))
-circuit_name.on_text("aa")
+
+x_draw_offset = 30
+y_draw_offset = 20
+circuit_store = pyg.shapes.Rectangle(x_draw_offset, y_draw_offset, screen_width - 2 * x_draw_offset,
+                                     80, color=(46, 45, 47, 255), batch=main_batch)
+
+circuit_name = pyg.gui.TextEntry("Name", x_draw_offset, screen_height - 100, screen_width - 2 * x_draw_offset,
+                                 color=(80, 78, 83, 255), batch=main_batch, text_color=(253, 255, 247, 255))
+
+draw_area = pyg.shapes.BorderedRectangle(x_draw_offset, y_draw_offset + 80 + 20, screen_width - 2 * x_draw_offset,
+                                         screen_height - 250, border=10, border_color=(111, 108, 113, 255),
+                                         color=(53, 52, 54, 255), batch=main_batch)
+
+
+key_handler = pyg.window.key.KeyStateHandler()
+mouse_handler = pyg.window.mouse.MouseStateHandler()
+"""
+mouse_handler notes:
+If a button is pressed then this handler holds a True value for it. If the window loses focus,
+all buttons will be reset to False in order to avoid a “sticky” button state.
+If holding down lmb:
+>>> mouse_handler[pyg.window.mouse.LEFT] == true
+>>> mouse_handler[pyg.window.mouse.RIGHT] == false
+"""
+
+app_window.push_handlers(key_handler)
+app_window.push_handlers(mouse_handler)
 
 
 a = and_circuit.AndCircuit('A1')
@@ -47,6 +70,11 @@ def update(dt):
     This is the main app loop, where objects will be checked and updated.
     :param dt: delta t, passed by pyg.clock.schedule_interval
     """
+    # if mouse_handler[pyg.window.mouse.LEFT]:
+    #     print("LMB")
+    # if mouse_handler[pyg.window.mouse.RIGHT]:
+    #     print("RMB")
+    if circuit_name.on_mouse_press()
     pass
 
 
